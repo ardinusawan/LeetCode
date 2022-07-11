3afa7d123104334e935cfee79fb2cd84196f2990
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2022-07-11 22:51:28 +0800

    Time: 5 ms (87.32%), Space: 4.3 MB (20.44%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.go
@@ -0,0 +1,40 @@
+var (
+    img [][]int
+    initColor int
+    newColor int
+    totalRow int
+    totalCol int
+)
+
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+    if image[sr][sc] == color {
+        return image
+    }
+    
+    img = image
+    initColor = image[sr][sc]
+    newColor = color
+    
+    totalRow = len(image)
+    totalCol = len(image[0])
+    paints(sr, sc)
+    return img
+}
+
+func paints(r, c int) {
+    if img[r][c] == initColor {
+        img[r][c] = newColor
+        if r >= 1 {
+            paints(r-1, c)
+        }
+        if r + 1 < totalRow {
+            paints(r+1, c)
+        }
+        if c >= 1 {
+            paints(r, c-1)
+        }
+        if c + 1 < totalCol {
+            paints(r, c+1)
+        }
+    }
+}
