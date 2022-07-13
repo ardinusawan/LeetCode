ea79b59b0fe8cbc56460d7d2ea819e5d6db7bd73
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2022-07-13 23:04:48 +0800

    Time: 11 ms (16.86%), Space: 2.9 MB (84.85%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.go
@@ -0,0 +1,13 @@
+func minCostClimbingStairs(cost []int) int {
+    for i:=len(cost)-3;i>=0;i--{
+        if cost[i + 1] < cost[i + 2] {
+            cost[i] += cost [i + 1]    
+        } else {
+            cost[i] += cost [i + 2]
+        }
+    }
+    if cost[0] < cost[1] {
+        return cost[0]
+    }
+    return cost[1]
+}
