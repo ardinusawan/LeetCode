ad4437b8732f95991a5521f2c8500c6afdee84d4
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-01 18:24:28 +0800

    Time: 13 ms, Memory: 3 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -5,22 +5,24 @@ import (
 
 func lengthOfLongestSubstring(s string) int {
     // sliding windows
+    // input: "pwwkew"
+    
+    // use the byte type as the key type for the charSet map, and store the index of each character in the map instead of the character itself. When a repeated character is encountered, we check if its index is greater than or equal to the left pointer. If it is, we update the left pointer to the next position after the index of the repeated character. Finally, we update the charSet map with the index of the current character, and update the result variable if necessary.
     
     var result, left int
-    charSet := make(map[byte]int)
+    charSet := make(map[byte]int) 
+    // key: char, value: index
+    
     for right:=0;right<len(s);right++ {
-        // for (charSet[s[right]]) {
-        //     delete(charSet, s[right])
-        //     left+=1
-        // }
+
         if index, found := charSet[s[right]]; found && index >= left {
-            // delete(charSet, s[right])
-            left = index + 1
+            left = index + 1 // left = 2
         }
-        // fmt.Println(index, found)
+        
         charSet[s[right]] = right
+ 
         result = int(math.Max(float64(result), float64(right - left + 1)))
     }
-    // fmt.Println(charSet)
+    
     return result
 }
