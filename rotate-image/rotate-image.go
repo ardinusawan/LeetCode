bc7f21fd3eec3d8d137b5bb85c0342a25a827e3d
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-05 00:24:13 +0800

    Time: 2 ms, Memory: 2.3 MB - LeetHub

--- rotate-image/rotate-image.go
+++ rotate-image/rotate-image.go
@@ -1,22 +1,16 @@
-import (
-    "fmt"
-)
-
 func rotate(matrix [][]int)  {
-	// have 4 pointer: left, right, top, bottom
-    // if matrix size more than 2x2, need we need to solve it recursively
-    // for every cycle ( 4 rotation ) left will increase, right will decrease, top increase, buttom decrease
-	
+	// have 4 pointer: left, right, top, bottom.
+    // matrix[row][colomn]
+    // if matrix size more than 2x2, need we need to solve it recursively by using left and right pointer. Increase left, decrease right so we will get diamond shape of matrix. Do it counterclockwise!
+    // In doing counterclockwise, only need to store matrix[top][left] in temporary object
+    // for every cycle ( 4 rotation ) left will increase, right will decrease, top increase, buttom decrease. Think like it diamond shape for second, third, ... etc iteration
+    
 	left, right := 0, len(matrix)-1 // colomn
 	for left < right {
 		top, buttom := left, right // row
 
         // iterate for every side of matrix
 		for i:=0;i<(right - left);i++ { 
-			// replace backward
-			// matrix[row][colomn]
-            // remember: decrease/increase with i counterclockwise!
-            // imagine diamond shape
 
             topLeft := matrix[top][left+i] // store first data temporary
 
