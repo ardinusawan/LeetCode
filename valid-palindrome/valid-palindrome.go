7482e3679fa59870301452c0ea0753ba182a4c98
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-05 09:35:27 +0800

    Time: 408 ms, Memory: 14.2 MB - LeetHub

--- valid-palindrome/valid-palindrome.go
+++ valid-palindrome/valid-palindrome.go
@@ -6,15 +6,13 @@ func isPalindrome(s string) bool {
     cleanString := ""
     for i:=0;i<len(s);i++ {
         v := s[i]
-        if v >= 65 && v <=90 || v >=97 && v<=122 {
+        if v >= 65 && v <=90 || v >=97 && v<=122 { // alphabet
             cleanString += string(unicode.ToLower(rune(v)))
-        } else if v >= 48 && v <= 57 {
+        } else if v >= 48 && v <= 57 { // numeric
             cleanString += string(v)
         }
     }
-    
-    // if len(cleanString) == 1 && len(s) != 1 { return false }
-    
+        
     left, right := 0, len(cleanString)-1
     for left <= right {
         if (cleanString[left] != cleanString[right]) {
