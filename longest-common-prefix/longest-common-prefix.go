47023369578bc7ab5f81193bfe79d6d58b28a7e3
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-13 00:25:23 +0800

    Time: 0 ms, Memory: 2.4 MB - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.go
@@ -0,0 +1,24 @@
+// use first array as comparison, NO NEED FIND SHORTEST WORD!
+// init result with empty string
+// for every element in first array (index: i)
+//   for every string in list
+//     if length of first array is out of bound || char is different then return result
+//     else continue
+//   append result with strs[0][i]
+
+func longestCommonPrefix(strs []string) string {
+    result := ""
+    if (len(strs) == 0) {
+        return result
+    }
+    
+    for i := range strs[0] { // flower
+        for _, s := range strs { // ["flower","flow","flight"]
+               if (i == len(s) || s[i] != strs[0][i]) {
+                return result
+            }
+        }
+        result += string(strs[0][i])
+    }
+    return result
+}
