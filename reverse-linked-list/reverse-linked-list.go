fec61ef4e1d9975e1b2bb23bfe2db3880dcf514b
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-24 08:35:09 +0800

    Time: 3 ms, Memory: 2.6 MB - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    current := head
+    for current != nil {
+        next := current.Next
+        current.Next = prev
+        prev = current
+        current = next
+    }
+    return prev
+}
