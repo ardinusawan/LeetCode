f5cbcbd738e67ffd20f3cbf0321a1ec76b01f0bd
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-04-26 21:22:04 +0800

    Time: 4 ms, Memory: 2.6 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.go
+++ merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -1,3 +1,9 @@
+// init result with empty node
+// iterate list1 and list2 using Next pointer
+// when one of it nil already, next is rest of the other one
+// use pointer to move between node
+// result.Next is the result
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,23 +13,23 @@
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
     result := &ListNode{Val: 0}
-    head := result
+    pointer := result
     
     for list1 != nil && list2 != nil {
         if list1.Val < list2.Val {
-            head.Next = list1
+            pointer.Next = list1
             list1 = list1.Next
         } else {
-            head.Next = list2
+            pointer.Next = list2
             list2 = list2.Next
         }
-        head = head.Next
+        pointer = pointer.Next
     }
     
     if list1 != nil {
-        head.Next = list1
+        pointer.Next = list1
     } else {
-        head.Next = list2
+        pointer.Next = list2
     }
     
     return result.Next
