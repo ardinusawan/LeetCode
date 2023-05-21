b845c2eb6ebdabfb6196b999eeecdec68ebe883b
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-05-22 01:14:36 +0800

    Time: 16 ms (35.10%), Space: 4.5 MB (24.31%) - LeetHub

--- permutation-in-string/permutation-in-string.go
+++ permutation-in-string/permutation-in-string.go
@@ -1,45 +1,58 @@
-// build 2 map, s1Map and s2Map
-// s1Map contains every char in s1, same with s2Map
-// left and right pointer start and end based on length of s1, in s2
-// only for first time, it need to compare s1Map and s2Map matches
-// after that, relay on var mathes. if matches in the end = 26 then it equal
 func checkInclusion(s1 string, s2 string) bool {
-	if len(s1) > len(s2) {
-		return false
+    if len(s1) > len(s2) {
+        return false
+    }
+
+    s1Count, s2Count := make(map[int]int, 26), make(map[int]int, 26)
+    for i := byte('a')-97; i <= byte('z'); i++ {
+		s1Count[int(i)-97] = 0
+        s2Count[int(i)-97] = 0
 	}
+    
+    for i := 0; i < len(s1); i++ {
+        s1Index := int(s1[i] - 'a')
+        s2Index := int(s2[i] - 'a')
+        s1Count[s1Index]++
+        s2Count[s2Index]++
+    }
 
-	s1Map, s2Map := make(map[byte]int, 26), make(map[byte]int, 26)
-	for i := 0; i < len(s1); i++ {
-		s1Map[s1[i]]++
-		s2Map[s2[i]]++
-	}
+    matches := 0
+    
+    for i:=0; i<26; i++ {
+        if s1Count[i] == s2Count[i] {
+            matches++
+        }
+    }
 
-	windowSize := len(s1)
-	for i := 0; i < len(s2)-windowSize; i++ {
-		if mapsMatch(s1Map, s2Map) {
-			return true
-		}
+    left := 0
+    for right:=len(s1); right<len(s2); right++ {
+        if matches == 26 {
+            return true
+        }
 
-		s2Map[s2[i]]--
-		if s2Map[s2[i]] == 0 {
-			delete(s2Map, s2[i])
-		}
-		s2Map[s2[i+windowSize]]++
-	}
+        index := int(s2[right] - 'a')
+        s2Count[index]++
+        if s1Count[index] == s2Count[index] { 
+            matches++
+        // } else {
+        //     matches--
+        // }
+        } else if s1Count[index] + 1 == s2Count[index] {
+            matches--
+        }
 
-	return mapsMatch(s1Map, s2Map)
-}
+        index2 := int(s2[left] - 'a')
+        s2Count[index2]--
+        if s1Count[index2] == s2Count[index2] { 
+            matches++
+        // } else {
+        //     matches--
+        // }
+        } else if s1Count[index2] - 1 == s2Count[index2] {
+            matches--
+        }
 
-func mapsMatch(m1, m2 map[byte]int) bool {
-	if len(m1) != len(m2) {
-		return false
-	}
-
-	for k, v := range m1 {
-		if m2[k] != v {
-			return false
-		}
-	}
-
-	return true
+        left++
+    }
+    return matches == 26
 }
