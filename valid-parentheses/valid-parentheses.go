e39093a4cd4ac97a8f80ead50b4c154c52eb2446
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-05-23 00:12:41 +0800

    Time: 2 ms (63.35%), Space: 2 MB (98.25%) - LeetHub

--- valid-parentheses/valid-parentheses.go
+++ valid-parentheses/valid-parentheses.go
@@ -1,3 +1,10 @@
+// create stack data structure with byte as value
+// implement Push, Pop, IsEmpty
+// when open bracket, push. 
+// when pair is correct, pop.
+// else, return false
+// in the end, if stack not empty return false
+
 type Stack []byte
 
 func (s *Stack) Push(value byte) {
@@ -27,10 +34,12 @@ func isValid(s string) bool {
             stack.Push(v)
             continue
         }
+
+        // below code is try to pop(). when it empty, nothing can be popped hence it must be fail
         if stack.IsEmpty() {
             return false
         }
-        
+
         if v == ')' && stack[len(stack)-1] == '(' {
             stack.Pop()
         } else if v == '}' && stack[len(stack)-1] == '{' {
