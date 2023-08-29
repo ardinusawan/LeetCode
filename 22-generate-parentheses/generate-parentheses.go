80dcbc70aa10d2aed3b87ca181d08f646a886b62
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2023-08-29 23:51:38 +0700

    Time: 1 ms (73.40%) | Memory: 2.6 MB (99.89%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.go
+++ 22-generate-parentheses/generate-parentheses.go
@@ -6,7 +6,6 @@ func generateParenthesis(n int) []string {
     // only add close paranthesis if close < open
     // valid if open == close == n
 
-
     stack = make([]string, 0) // reset
     result = make([]string, 0) // reset
     backtracking(n, 0, 0)
@@ -29,14 +28,12 @@ func backtracking(n, open, close int) {
     if open < n {
         stack = append(stack, "(")
         backtracking(n, open + 1, close)
-        stack = stack[:len(stack)-1] // pop
-        // fmt.Println("1", stack) // to visualize
+        stack = stack[:len(stack)-1] // after stack is joined and to result, it need to find other combination if any
     }
 
     if close < open {
         stack = append(stack, ")")
         backtracking(n, open, close + 1)
-        stack = stack[:len(stack)-1] // pop
-        // fmt.Println("2", stack) // to visualize
+        stack = stack[:len(stack)-1] // after stack is joined and to result, it need to find other combination if any
     }
 }
